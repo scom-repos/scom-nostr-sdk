1e48195142e414e35fb48fa60bf82bab2568fe81
johnnycheuk <johnnycheuk2649@gmail.com>
2024-04-09 11:31:32 +0800

    Crypto related files from scom signerg

--- types/utils/keys.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function generatePrivateKey(): string;
-export declare function getPublicKey(privateKey: string): string;
-export declare function getPublicKeyY(privateKey: string): string;
-export declare function getSharedSecret(privateKey: string, publicKey: string): string;
-export declare function decompressPublicKey(publicKey: string): string;
