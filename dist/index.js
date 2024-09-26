d728b11169313cf75ddc05daa2333aa8b0638778
ojochau <ojochau@instajs.com>
2024-09-26 15:44:47 +0800

    Add NetworkType to ISubscriptionDiscountRule

--- dist/index.js
+++ dist/index.js
@@ -3530,7 +3530,7 @@ define("@scom/scom-social-sdk/core/index.ts", ["require", "exports", "@scom/scom
 define("@scom/scom-social-sdk/utils/interfaces.ts", ["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.CalendarEventType = exports.CommunityRole = exports.PaymentModel = exports.ProtectedMembershipPolicyType = exports.MembershipType = exports.ScpStandardId = exports.TokenType = exports.NftType = void 0;
+    exports.CalendarEventType = exports.CommunityRole = exports.NetworkType = exports.PaymentModel = exports.ProtectedMembershipPolicyType = exports.MembershipType = exports.ScpStandardId = exports.TokenType = exports.NftType = void 0;
     var NftType;
     (function (NftType) {
         NftType["ERC721"] = "ERC721";
@@ -3565,6 +3565,12 @@ define("@scom/scom-social-sdk/utils/interfaces.ts", ["require", "exports"], func
         PaymentModel["OneTimePurchase"] = "OneTimePurchase";
         PaymentModel["Subscription"] = "Subscription";
     })(PaymentModel = exports.PaymentModel || (exports.PaymentModel = {}));
+    var NetworkType;
+    (function (NetworkType) {
+        NetworkType["EVM"] = "EVM";
+        NetworkType["TON"] = "TON";
+        NetworkType["Telegram"] = "Telegram";
+    })(NetworkType = exports.NetworkType || (exports.NetworkType = {}));
     var CommunityRole;
     (function (CommunityRole) {
         CommunityRole["Creator"] = "creator";
@@ -3581,10 +3587,11 @@ define("@scom/scom-social-sdk/utils/interfaces.ts", ["require", "exports"], func
 define("@scom/scom-social-sdk/utils/index.ts", ["require", "exports", "@scom/scom-social-sdk/utils/interfaces.ts"], function (require, exports, interfaces_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.CalendarEventType = exports.CommunityRole = exports.ProtectedMembershipPolicyType = exports.PaymentModel = exports.MembershipType = exports.TokenType = void 0;
+    exports.CalendarEventType = exports.CommunityRole = exports.ProtectedMembershipPolicyType = exports.NetworkType = exports.PaymentModel = exports.MembershipType = exports.TokenType = void 0;
     Object.defineProperty(exports, "TokenType", { enumerable: true, get: function () { return interfaces_1.TokenType; } });
     Object.defineProperty(exports, "MembershipType", { enumerable: true, get: function () { return interfaces_1.MembershipType; } });
     Object.defineProperty(exports, "PaymentModel", { enumerable: true, get: function () { return interfaces_1.PaymentModel; } });
+    Object.defineProperty(exports, "NetworkType", { enumerable: true, get: function () { return interfaces_1.NetworkType; } });
     Object.defineProperty(exports, "ProtectedMembershipPolicyType", { enumerable: true, get: function () { return interfaces_1.ProtectedMembershipPolicyType; } });
     Object.defineProperty(exports, "CommunityRole", { enumerable: true, get: function () { return interfaces_1.CommunityRole; } });
     Object.defineProperty(exports, "CalendarEventType", { enumerable: true, get: function () { return interfaces_1.CalendarEventType; } });
@@ -11572,7 +11579,7 @@ define("@scom/scom-social-sdk/managers/index.ts", ["require", "exports", "@scom/
 define("@scom/scom-social-sdk", ["require", "exports", "@scom/scom-social-sdk/core/index.ts", "@scom/scom-social-sdk/utils/index.ts", "@scom/scom-social-sdk/managers/index.ts"], function (require, exports, index_8, index_9, managers_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NostrRestAPIManager = exports.NostrWebSocketManager = exports.SocialDataManager = exports.SocialUtilsManager = exports.NostrEventManagerWrite = exports.NostrEventManagerReadV2 = exports.NostrEventManagerRead = exports.CalendarEventType = exports.CommunityRole = exports.ProtectedMembershipPolicyType = exports.PaymentModel = exports.MembershipType = exports.TokenType = exports.schnorr = exports.secp256k1 = exports.Bech32 = exports.Nip19 = exports.Keys = exports.Event = void 0;
+    exports.NostrRestAPIManager = exports.NostrWebSocketManager = exports.SocialDataManager = exports.SocialUtilsManager = exports.NostrEventManagerWrite = exports.NostrEventManagerReadV2 = exports.NostrEventManagerRead = exports.CalendarEventType = exports.CommunityRole = exports.ProtectedMembershipPolicyType = exports.NetworkType = exports.PaymentModel = exports.MembershipType = exports.TokenType = exports.schnorr = exports.secp256k1 = exports.Bech32 = exports.Nip19 = exports.Keys = exports.Event = void 0;
     Object.defineProperty(exports, "Event", { enumerable: true, get: function () { return index_8.Event; } });
     Object.defineProperty(exports, "Keys", { enumerable: true, get: function () { return index_8.Keys; } });
     Object.defineProperty(exports, "Nip19", { enumerable: true, get: function () { return index_8.Nip19; } });
@@ -11582,6 +11589,7 @@ define("@scom/scom-social-sdk", ["require", "exports", "@scom/scom-social-sdk/co
     Object.defineProperty(exports, "TokenType", { enumerable: true, get: function () { return index_9.TokenType; } });
     Object.defineProperty(exports, "MembershipType", { enumerable: true, get: function () { return index_9.MembershipType; } });
     Object.defineProperty(exports, "PaymentModel", { enumerable: true, get: function () { return index_9.PaymentModel; } });
+    Object.defineProperty(exports, "NetworkType", { enumerable: true, get: function () { return index_9.NetworkType; } });
     Object.defineProperty(exports, "ProtectedMembershipPolicyType", { enumerable: true, get: function () { return index_9.ProtectedMembershipPolicyType; } });
     Object.defineProperty(exports, "CommunityRole", { enumerable: true, get: function () { return index_9.CommunityRole; } });
     Object.defineProperty(exports, "CalendarEventType", { enumerable: true, get: function () { return index_9.CalendarEventType; } });
